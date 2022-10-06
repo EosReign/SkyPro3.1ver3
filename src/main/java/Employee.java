ff85b15338cc28b1cfe3bc3a451ee136eecf04b8
EosReign <107928418+EosReign@users.noreply.github.com>
2022-10-06 23:17:11 +0300

    .

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -4,18 +4,23 @@ public class Employee {
     private String fullName;
     private int department;
     private double salary;
-    private final int id;
+    private int id;
     static int countId = 0;
 
-    public Employee(String fullName, int department, double salary) throws BadRequestException{
+    public Employee(String fullName, int department, double salary) throws BadRequestException {
         if (StringUtils.isWhitespace(fullName)) {
-            throw new BadRequestException("Имя и фамилия пусты!");
-        } else if (StringUtils.is)
-        this.fullName = fullName;
-        this.department = department;
-        this.salary = salary;
-        this.id = countId++;
+            throw new BadRequestException("400 Bad Request");
+        } else if (StringUtils.isAllUpperCase(fullName)) {
+            throw new BadRequestException("400 Bad Request");
+        } else if (StringUtils.isAllLowerCase(fullName)) {
+            fullName = StringUtils.capitalize(fullName);
+        } else {
+            this.fullName = fullName;
+            this.department = department;
+            this.salary = salary;
+            this.id = countId++;
         }
+    }
     public String getFullName() {
         return fullName;
     }
